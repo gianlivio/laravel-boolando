807df76312a883701054542ec8d4972f03910986
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-06-08 12:00:16 +0200

    add header icons + dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        
+        <!-- Fontawesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
         <!-- Styles -->
 
         {{-- assets con direttiva vite --}}
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,13 @@
 <header>
 
-    <ul>
+    {{-- sezione sinistra con menu  --}}
+
+    <ul class="d-flex gap-2">
 
         <li>
 
             <a href="{{ route("donna") }}">
-
                 Donna
-
             </a>
 
         </li>
@@ -15,9 +15,7 @@
         <li>
 
             <a href="{{ route("uomo") }}">
-
                 Uomo
-
             </a>
 
         </li>
@@ -25,23 +23,92 @@
         <li>
 
             <a href="{{ route("bambini") }}">
-
                 Bambini
-
             </a>
 
         </li>
 
     </ul>
 
-    
+
+    {{-- sezione centrale, logo --}}
+
     <div class="text-center">
 
         <a href="{{ route("home") }}">
 
-            <img src="{{ Vite::asset("resources/img/boolean-logo.png")}}" alt="loogo boolean">
+            <img class="logo" src="{{ Vite::asset("resources/img/boolean-logo.png")}}" alt="loogo boolean">
 
         </a>
+
+    </div>
+
+    {{-- sezione icone --}}
+
+    <div class="d-flex gap-2">
+
+        <div class="icon-container">
+
+            {{-- icona user --}}
+
+            <a href="" id="userIcon" class="icon">
+
+                <i class="fa fa-user"></i>
+
+            </a>
+
+            {{-- menu a tendina per icona user --}}
+
+            <div class="dropdown-content">
+
+                <a href="">Profilo</a>
+                <a href="">Impostazioni</a>
+                <a href="">Esci</a>
+             
+            </div>
+
+        </div>
+
+        {{-- icona favorites --}}
+
+        <div class="icon-container">
+
+            <a href="" id="favoritesIcon" class="icon">
+
+                <i class="fa fa-heart"></i>
+
+            </a>
+
+            {{-- menu a tendina preferiti --}}
+
+            <div class="dropdown-content">
+
+                <a href="">Preferiti</a>
+
+               
+            </div>
+
+        </div>
+
+        {{-- icona carrello --}}
+
+        <div class="icon-container">
+
+            <a href="">
+
+                <i class="fa fa-shopping-cart"></i>
+
+            </a>
+
+            {{-- menu a tendina per carrello --}}
+            <div class="dropdown-content">
+
+                <a href="">Vai al carrello</a>
+                <a href="">Acquista subito</a>
+                
+            </div>
+
+        </div>
 
     </div>
  
